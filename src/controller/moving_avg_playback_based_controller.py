0e6fd7d25e028f46adb6a90e515f7dfb96234b31
fourcolor <james1qaz2wsx12qw@gmail.com>
2025-03-18 16:32:21 +0800

    Fix std error

--- src/controller/moving_avg_playback_based_controller.py
+++ src/controller/moving_avg_playback_based_controller.py
@@ -12,16 +12,19 @@ class Moving_Average_Playback_Controller(Controller):
     def __init__(self, udp_traffic_csv, interface, rate_mbit=1000, burst_mbit=100, latency_ms=5, resample_interval = 0.3, rolling_wnd_size = 1):
         super().__init__(interface, rate_mbit, burst_mbit, latency_ms)
         data = pd.read_csv(udp_traffic_csv)
-        data['tx_time_epoch'] = pd.to_datetime(data['tx_time_epoch'], unit='s')
+        data['tx_time_epoch'] = pd.to_datetime(data['tx_time_epoch'], unit='s') - pd.Timedelta(hours=8)
         latency = data.dropna(subset=['latency']).set_index('tx_time_epoch')['latency'].sort_index()
+        std = latency.rolling(f'{rolling_wnd_size}s').std()
         latency = latency.rolling(f'{rolling_wnd_size}s').mean()
-        latency = latency.resample(f'{resample_interval}S')
-        latency = pd.DataFrame({
-            "mean_latency": latency.ffill(),
-            "std_latency": latency.std()    
+        latency_rs = latency.resample(f'{resample_interval}S')
+        std_rs = std.resample(f'{resample_interval}S')
+        latency_final = pd.DataFrame({
+            "mean_latency": latency_rs.ffill(),
+            "std_latency": std_rs.ffill()    
         })
+
         lost = data.set_index('tx_time_epoch')['lost'].resample(f'{resample_interval}S').agg(['mean'])
-        self.result = pd.concat([latency, lost], axis=1)
+        self.result = pd.concat([latency_final, lost], axis=1)
         self.result.columns = ['mean_latency', 'std_latency', 'mean_lost']
         self.result['mean_latency'] = self.result['mean_latency'] * 1000 
         self.result['std_latency'] = self.result['std_latency'] * 1000 
@@ -29,6 +32,7 @@ class Moving_Average_Playback_Controller(Controller):
         
         self.waiting_time = 0
         self.interface = interface
+        self.resample_interval = resample_interval
         self.start_log_time = self.result.index[0].timestamp()
 
     def set_waiting_time(self, waiting_time):
@@ -38,7 +42,9 @@ class Moving_Average_Playback_Controller(Controller):
         start_time = time.time()
         start_log_time = self.result.index[0].timestamp()
         for row in self.result.itertuples():
-            if (row.Index.timestamp() - start_log_time + self.waiting_time) - (time.time() - start_time) > 0.05:
+            if (row.Index.timestamp() - start_log_time + self.waiting_time) - (time.time() - start_time) < self.resample_interval:
+                continue
+            if (row.Index.timestamp() - start_log_time + self.waiting_time) - (time.time() - start_time) > 0:
                 time.sleep((row.Index.timestamp() - start_log_time + self.waiting_time) - (time.time() - start_time))
             self.run_netem_cmd(row.mean_lost*100, row.mean_latency, row.std_latency, 'normal', self.interface)
             print(f"Index: {row.Index.timestamp()}, Mean Latency: {row.mean_latency}, STD Latency: {row.std_latency}, Lost Ratio: {row.mean_lost}")
